ee43dd21e6efa07fe6c3f6752c673cb7c5e681a6
JoannaZhang-cs <jzsunset10@gmail.com>
2026-03-26 11:51:35 -0400

    Update Location.vue

---
+++ vue-project/src/components/Location.vue
@@ -0,0 +1,35 @@
+<template>
+  <router-link :to="locationPath" class="card">
+    <h2>{{ location.name }}</h2>
+  </router-link>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const { location } = defineProps({
+  location: {
+    type: Object,
+    required: true,
+  },
+})
+const locationPath = computed(() => `/location/${location.id}`)
+</script>
+
+<style scoped>
+.card {
+  width: 28%;
+  height: 500px;
+  background-color: aliceblue;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  text-transform: uppercase;
+}
+img {
+  width: 100%;
+  height: 80%;
+  object-fit: cover;
+}
+</style>
